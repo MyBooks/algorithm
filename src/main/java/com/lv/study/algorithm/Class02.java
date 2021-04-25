0b01dc02f1d5dd4f9891236acb2301957f0a86ae
MyBooks <1154712196@qq.com>
2021-04-25 22:05:21 +0800

    链表

--- src/main/java/com/lv/study/algorithm/Class02.java
+++ src/main/java/com/lv/study/algorithm/Class02.java
@@ -1,10 +1,32 @@
 package com.lv.study.algorithm;
 
 /**
- *
+ * 链表问题（单双链表）
  *
  * @author lvxiaoqiang
  * @since 2021/4/23 9:14 下午
  */
 public class Class02 {
+
+    /**
+     * 单链表节点
+     */
+    public class Node{
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(){
+
+        }
+
+
+        int value;
+        Node next;
+    }
+
+    public static void main(String[] args) {
+        // 问题1：单链表
+    }
 }
